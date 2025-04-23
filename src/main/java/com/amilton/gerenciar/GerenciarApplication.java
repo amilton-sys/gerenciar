9f2abacc60b5bfeadf36870c62613e092b205f45
Amilton <amiltonjose.pe@gmail.com>
2025-04-23 16:44:51 -0300

    Cria estrutura inicial do projeto

---
+++ src/main/java/com/amilton/gerenciar/GerenciarApplication.java
@@ -0,0 +1,13 @@
+package com.amilton.gerenciar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarApplication.class, args);
+	}
+
+}
